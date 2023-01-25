65ed6e1db333f79a5f6f64a4ba98b25e3b3eece6
Abbas Aliyev <alievabbas1@gmail.com>
2023-01-26 00:04:05 +0400

    Usage framework

--- main.c
+++ main.c
@@ -16,16 +16,48 @@ void compress(FILE *input, FILE *output){
   
 }
 
-int main() {
-  FILE *input = fopen("input1.txt", "rb");
-  if(!input) exit(1);
+void decompress(FILE *input, FILE *output){
+  int prefix = getc(input);
+  if(prefix == EOF) return;
+  
+  int character;
   
   
   
   
   
+}
+
+void error(char *message){
+  fprintf(stderr, "%s\n", message);
+  exit(1);
+}
+
+int main(int argc, char **argv) {
+  if(argc != 3) error("Usage: ./lzw [-c|-d] input_file output_file");
+  if(argv[1][0] != '-' || (argv[1][1] != 'c' && argv[1][1] != 'd')) error("Usage: ./lzw [-c|-d] input_file output_file");
   
-  
-  
+  if(argv[1][1] == 'c'){
+    FILE *input = fopen(argv[2], "rb");
+    if(!input) error("Error: Could not open input file");
+    FILE *output = fopen(argv[3], "wb");
+    if(!output) error("Error: Could not open output file");
+    
+    compress(input, output);
+    
+    fclose(input);
+    fclose(output);
+  }
+  else if(argv[1][1] == 'd'){
+    FILE *input = fopen(argv[2], "rb");
+    if(!input) error("Error: Could not open input file");
+    FILE *output = fopen(argv[3], "wb");
+    if(!output) error("Error: Could not open output file");
+    
+    decompress(input, output);
+    
+    fclose(input);
+    fclose(output);
+  }
   return 0;
 }
